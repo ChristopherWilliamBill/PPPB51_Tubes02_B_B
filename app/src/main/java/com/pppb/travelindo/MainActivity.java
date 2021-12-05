32cc5cf8386aa2c67bca5950e53747e4fcf9ad41
ChristopherWilliam6181901048 <christ.william.bill@gmail.com>
2021-12-05 23:30:50 +0700

    Membuat splashscreen

--- app/src/main/java/com/pppb/travelindo/MainActivity.java
+++ app/src/main/java/com/pppb/travelindo/MainActivity.java
@@ -2,15 +2,28 @@ package com.pppb.travelindo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+        this.handler = new Handler();
+        this.handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
 
+            }
+        }, 2000);
     }
 }
